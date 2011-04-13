e9336e06abe47adc4e0e07698615d01201ed4e20
ungelivable <xyc0562@gmail.com>
2011-04-13 01:54:06 -0700

    make ac variables local so that auto-complete functions normally in non-c modes.

--- sample-config.el
+++ sample-config.el
@@ -2,10 +2,12 @@
 (setq clang-completion-suppress-error 't)
 
 (defun my-c-mode-common-hook()
+  (make-local-variable 'ac-auto-start)
+  (make-local-variable 'ac-expand-on-auto-complete)
+  (make-local-variable 'ac-quick-help-delay)
   (setq ac-auto-start nil)
   (setq ac-expand-on-auto-complete nil)
   (setq ac-quick-help-delay 0.3)
-  (define-key c-mode-base-map (kbd "M-/") 'ac-complete-clang)
+  (define-key c-mode-base-map (kbd "M-?") 'ac-complete-clang)
 )
-
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook
